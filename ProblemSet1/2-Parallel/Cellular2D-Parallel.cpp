fd2e613fc87809d12f7ff64f6a5ce0116d609402
howiemalowie <havard.notland@gmail.com>
2019-02-11 05:06:25 -0800

    Reworked parallel-2

--- ProblemSet1/2-Parallel/Cellular2D-Parallel.cpp
+++ ProblemSet1/2-Parallel/Cellular2D-Parallel.cpp
@@ -7,42 +7,171 @@
 #include<string>
 #include<fstream>
 #include<cmath>
+#include<iostream>
 
 using namespace std;
 
-void buildString(char* builtString, char partition[], int counter, int x, int y, int dim){
-
-    //TODO: Create string of nearby elements
-    char up,down,left,right,upleft,downright,upright,downleft;
+void buildString(char* builtString, vector<vector<char>> myLines, vector<char> firstLine,
+        vector<char> lastLine, int x, int y, int dim){
 
 
+    //Control edge cases
     if(x==0){
         if(y==0){
+            builtString[0] = firstLine[dim-1];
+            builtString[1] = firstLine[y];
+            builtString[2] = firstLine[y+1];
 
+            builtString[3] = myLines[x][dim-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
+
+            builtString[6] = myLines[x+1][dim-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][y+1];
+        }
+        else if(y==dim-1){
+            builtString[0] = firstLine[y-1];
+            builtString[1] = firstLine[y];
+            builtString[2] = firstLine[0];
+
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][0];
+
+            builtString[6] = myLines[x+1][y-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][0];
+        }
+        else {
+            builtString[0] = firstLine[y - 1];
+            builtString[1] = firstLine[y];
+            builtString[2] = firstLine[y + 1];
+
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
+
+            builtString[6] = myLines[x+1][y-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][y+1];
         }
     }
+    else if(x == myLines.size()-1){
+        if(y==0){
+            builtString[0] = myLines[x-1][dim-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][y+1];
 
+            builtString[3] = myLines[x][dim-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
 
+            builtString[6] = lastLine[dim-1];
+            builtString[7] = lastLine[y];
+            builtString[8] = lastLine[y+1];
+        }
+        else if(y==dim-1){
+            builtString[0] = myLines[x-1][y-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][0];
 
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][0];
 
+            builtString[6] = lastLine[y-1];
+            builtString[7] = lastLine[y];
+            builtString[8] = lastLine[0];
+        }
+        else {
+            builtString[0] = myLines[x-1][y-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][y+1];
+
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
+
+            builtString[6] = lastLine[y - 1];
+            builtString[7] = lastLine[y];
+            builtString[8] = lastLine[y + 1];
+        }
+
+    } else{
+        if(y==0){
+            builtString[0] = myLines[x-1][dim-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][y+1];
+
+            builtString[3] = myLines[x][dim-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
+
+            builtString[6] = myLines[x+1][dim-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][y+1];
+        }
+        else if(y == dim-1){
+            builtString[0] = myLines[x-1][y-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][0];
+
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][0];
+
+            builtString[6] = myLines[x+1][y-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][0];
+        }
+        else{
+            builtString[0] = myLines[x-1][y-1];
+            builtString[1] = myLines[x-1][y];
+            builtString[2] = myLines[x-1][y+1];
+
+            builtString[3] = myLines[x][y-1];
+            builtString[4] = myLines[x][y];
+            builtString[5] = myLines[x][y+1];
+
+            builtString[6] = myLines[x+1][y-1];
+            builtString[7] = myLines[x+1][y];
+            builtString[8] = myLines[x+1][y+1];
+        }
+    }
 }
 
-int main(int argc, char** argv){
+int binConv(char* binary)
+{
+    int decimal = 0;
+    int indexCounter = 0;
+    cout << "Binary: " << binary;
 
-    const int ruleLength = 11;
+    //TODO: CONVERT TO DECIMAL
+    for(int i = 8; i >= 0; i--)
+    {
+
+        if(binary[i] == '1')
+        {
+            decimal += pow(2, indexCounter);
+        }
+        indexCounter++;
+    }
+
+    printf(" Decimal: %d", decimal);
+    cout << '\n';
+    return (decimal);
+}
+
+int main(int argc, char **argv){
+    printf("Initializing");
     int my_rank;
     int comm_sz;
     int dim;
-    int rules_size, board_size;
-    int* sendbuf;
     int part;
-    int xStart;
-    int yStart;
-    int xEnd;
-    int yEnd;
     vector<vector<char>> rules;
     vector<vector<char>> board;
-    char temp_board[dim*dim];
+    vector<vector<char>> myLines;
 
     string file1 = argv[1];
     string file2 = argv[2];
@@ -53,19 +182,20 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 
+    //All processes read the rules
+    ifstream infile1(file1);
+    string line1;
+
+    while(getline(infile1, line1)){
+        vector<char> line1_char(line1.begin(), line1.end());
+        rules.push_back(line1_char);
+    }
+    infile1.close();
+
+
     if(my_rank == 0){
-        ifstream infile1(file1);
-        string line1;
 
-        while(getline(infile1, line1)){
-            vector<char> line1_char(line1.begin(), line1.end());
-            rules.push_back(line1_char);
-        }
-        infile1.close();
-
-        rules_size = rules.size();
-
-
+        //Root process reads board state
         ifstream infile2(file2);
         string line2;
 
@@ -78,74 +208,137 @@ int main(int argc, char** argv){
         }
         infile2.close();
 
-        //Create temporary 1D-version of board
+        part = dim/comm_sz;
 
-        int counter = 0;
-        for(vector<char> v : board){
-            for(char c : v){
-                temp_board[counter] = c;
-                counter++;
-            }
-        }
-
-        part = dim*dim/comm_sz;
-
-
+        //Broadcast how many lines each process receives
         MPI_Bcast(&part, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&rules_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&rules[0][0], rules_size, MPI_CHAR, 0, MPI_COMM_WORLD);
     }
 
     else{
         MPI_Bcast(&part, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&rules_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        rules.resize(rules_size, vector<char>(ruleLength));
-        MPI_Bcast(&rules[0][0], rules_size, MPI_CHAR, 0, MPI_COMM_WORLD);
     }
 
-    sendbuf = (int *)malloc(part*sizeof(char));
-    MPI_Scatter(&temp_board, part, MPI_CHAR, sendbuf, part, MPI_CHAR, 0, MPI_COMM_WORLD);
+    for(int k=0; k<t; k++){
 
-    //Find the x and y coordinate on the board for each partition's start and end point
-    dim = sqrt(part*comm_sz);
-    xStart = my_rank * part/dim;
-    yStart = (my_rank * part) % dim;
-    xEnd = xStart + part/dim;
-    yEnd = (yStart + part-1) % dim;
+        if(my_rank == 0){
+            //Give lines to root process
+            for(int i=0; i<part; i++){
+                myLines.push_back(board.at(i));
+            }
 
-    int x = xStart;
-    int y = yStart;
-    char newString[part];
-    char builtString[9];
-    int counter = 0;
-    for(char c : temp_board){
 
-        char up=c, down=c, left=c, right=c;
-        int rankOfProcessBelow = my_rank + (dim+counter)/part;
-        if(rankOfProcessBelow > comm_sz-1){
-            rankOfProcessBelow -= comm_sz;
-        }
-        int rankOfProcessAbove = my_rank - (dim/part);
-        if(rankOfProcessAbove < 0){
-            rankOfProcessAbove += comm_sz;
-        }
-        MPI_Send(&up, 1, MPI_CHAR, rankOfProcessBelow, 0, MPI_COMM_WORLD);
-        MPI_Send(&down, 1, MPI_CHAR, rankOfProcessAbove, 0, MPI_COMM_WORLD);
+            //Distribute lines to other processes
+            int lineToSend = part;
+            vector<char> line;
+            for(int i=1; i<comm_sz-1; i++){
+                for(int j=0; j<part; j++) {
+                    line = board[lineToSend];
+                    cout << "Trying to send to process" << i;
+                    MPI_Send(&line[0], dim, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+                    lineToSend++;
+                }
+            }
 
-        if(counter==0 && comm_sz != 1){
+            //Send remaining lines to last process
+            while(lineToSend < dim){
+                line = board[lineToSend];
+                cout << "Trying to send to process" << comm_sz-1;
+                MPI_Send(&line[0], dim, MPI_CHAR, comm_sz-1, 0, MPI_COMM_WORLD);
+                lineToSend++;
+            }
 
         }
 
-        buildString(builtString, temp_board, counter, x, y, dim);
-
-        if(y == dim-1){
-            x++;
-            y = 0;
-        }
         else{
-            y++;
+            int dim = part * comm_sz;
+            vector<char> line(dim);
+            for (int j = 0; j < part; j++) {
+                MPI_Recv(&line[0], dim, MPI_CHAR, my_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                myLines.push_back(line);
+            }
+
+
+            if (my_rank == comm_sz - 1) {
+                for (int i = 0; i < dim % comm_sz; i++) {
+                    MPI_Recv(&line[0], dim, MPI_CHAR, my_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    myLines.push_back(line);
+                }
+            }
         }
-        counter++;
+
+
+        //Send and receive neighbour lines
+        vector<char> lastLine = myLines[0];
+        vector<char> firstLine = myLines.back();
+        int rankAbove = my_rank-1;
+        int rankBelow = my_rank+1;
+
+        if(my_rank == 0){
+            rankAbove = comm_sz-1;
+        }
+        else if(my_rank == comm_sz-1){
+            rankBelow = 0;
+        }
+
+        cout << "Trying to send to process" << rankBelow;
+        MPI_Send(&firstLine[0], dim, MPI_CHAR, rankBelow, 0, MPI_COMM_WORLD);
+        cout << "Trying to send to process" << rankAbove;
+        MPI_Send(&lastLine[0], dim, MPI_CHAR, rankAbove, 0, MPI_COMM_WORLD);
+
+        MPI_Recv(&firstLine[0], dim, MPI_CHAR, rankAbove, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&lastLine[0], dim, MPI_CHAR, rankBelow, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+        //Update cells
+        vector<vector<char>> newLines = myLines;
+        int bin;
+        char builtString[9];
+        for(int i=0; i<myLines.size(); i++){
+            for(int j=0; j<dim; j++){
+                buildString(builtString, myLines, firstLine, lastLine, i, j, dim);
+
+                bin = binConv(builtString);
+
+                newLines[i][j] = rules[bin].back();
+            }
+        }
+
+        vector<char> line(dim);
+        if(my_rank == 0){
+            //Receive updated lines
+            for(int i=1; i<comm_sz-1; i++){
+                for(int j=0; j<part; j++) {
+                    MPI_Recv(&line[0], dim, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    newLines.push_back(line);
+                }
+            }
+
+            //Receive additional lines from last process
+            for(int i=0; i<dim%comm_sz; i++){
+                MPI_Recv(&line[0], dim, MPI_CHAR, comm_sz-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                newLines.push_back(line);
+            }
+
+            //Print state
+            for(int i=0; i<dim; i++){
+                for(int j=0; j<dim; j++){
+                    cout << newLines[i][j];
+                }
+                cout << '\n';
+            }
+
+            board = newLines;
+        }
+
+
+        else{
+            //Send updated cells to root
+            for(int i=0; i<newLines.size(); i++){
+                line = newLines[i];
+                MPI_Send(&line[0], dim, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+            }
+
+        }
     }
 
 
